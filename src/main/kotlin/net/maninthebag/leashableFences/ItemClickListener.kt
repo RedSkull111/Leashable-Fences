c1616391d20000946999bf7facb8c76d92b4a45e
RedSkull111 <106792748+RedSkull111@users.noreply.github.com>
2025-05-17 17:01:56 +1000

    Add files via upload

--- src/main/kotlin/net/maninthebag/leashableFences/ItemClickListener.kt
+++ src/main/kotlin/net/maninthebag/leashableFences/ItemClickListener.kt
@@ -1,5 +1,6 @@
 package net.maninthebag.leashableFences
 
+import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.Bat
@@ -13,16 +14,21 @@ import org.bukkit.event.entity.EntityUnleashEvent
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
-import java.util.*
+import org.bukkit.util.BoundingBox
+import kotlin.collections.get
 
 class ItemClickListener(private val plugin: JavaPlugin) : Listener {
 
-    private val activeLeashes = mutableMapOf<UUID, Bat>()
+    private val activeLeashes = mutableMapOf<LeashHitch, Bat>()
     private val activeBats = mutableMapOf<Bat, LeashHitch>()
 
     private val leashMap = mutableMapOf<Player, LeashHitch>()
+
+    private val playerMap = mutableMapOf<Bat, Player>()
+    private val batMap = mutableMapOf<Player, Bat>()
 
     private val leashes = mutableMapOf<Location, LeashHitch>()
 
@@ -34,14 +40,26 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
         val clicked = event.clickedBlock ?: return
 
         if (player.isSneaking) return
-        if (event.action != Action.RIGHT_CLICK_BLOCK || event.clickedBlock?.type?.name?.endsWith("_FENCE") == false && event.clickedBlock?.type?.name?.endsWith("_WALL") == false) return
-        if (event.item?.type != Material.LEAD) return
+        if (event.action != Action.RIGHT_CLICK_BLOCK || event.clickedBlock?.type?.name?.endsWith("_FENCE") == false) return
+
+        if (event.item?.type != Material.LEAD && leashMap[event.player] == null) return
 
         event.isCancelled = true
 
         val leashLoc = clicked.location.add(0.5,0.0,0.5)
 
         if (!leashMap.containsKey(player)) {
+            val loc = clicked.location
+            val boundingBox = BoundingBox(
+                loc.x, loc.y, loc.z,
+                loc.x + 1, loc.y + 1, loc.z + 1
+            )
+
+            val areaLeashes = clicked.world.getNearbyEntities(boundingBox).filterIsInstance<LeashHitch>()
+            for (leash in areaLeashes) {
+                leash.remove()
+            }
+
             val hitch = clicked.world.spawn(leashLoc, LeashHitch::class.java)
 
             val bat = clicked.world.spawn(leashLoc, Bat::class.java) {
@@ -52,9 +70,20 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
                 it.leashHolder = hitch
             }
 
-            activeLeashes[hitch.uniqueId] = bat
+            activeLeashes[hitch] = bat
             leashMap[player] = hitch
             leashes[hitch.location] = hitch
+            playerMap[bat] = player
+            batMap[player] = bat
+            if (player.gameMode != GameMode.CREATIVE) {
+                for (itemStack in player.inventory) {
+                    if (itemStack == null || itemStack.type != Material.LEAD) {
+                        continue
+                    }
+                    itemStack.amount = itemStack.amount - 1
+                    break
+                }
+            }
 
             object  : BukkitRunnable() {
                 override fun run() {
@@ -69,10 +98,10 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
             }.runTaskTimer(plugin, 0L, 1L)
         } else {
             val firstLoc = leashMap.remove(player) ?: return
-            activeLeashes[firstLoc.uniqueId]?.remove()
+            activeLeashes[firstLoc]?.remove()
 
             val hitch1 = clicked.world.spawn(firstLoc.location, LeashHitch::class.java)
-            val hitch2 = clicked.world.spawn(leashLoc, LeashHitch::class.java)
+            clicked.world.spawn(leashLoc, LeashHitch::class.java)
 
             val bat = clicked.world.spawn(leashLoc, Bat::class.java) {
                 it.isInvisible = true
@@ -80,6 +109,8 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
                 it.setAI(false)
                 it.isInvulnerable = true
                 it.setLeashHolder(hitch1)
+                it.isPersistent = true
+                it.removeWhenFarAway = false
             }
 
             bat.teleport(leashLoc)
@@ -90,22 +121,20 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
 
             bat.teleport(loc)
 
-            activeLeashes[hitch1.uniqueId] = bat
+            activeLeashes[hitch1] = bat
             activeBats[bat] = hitch1
+            playerMap.remove(batMap[player])
 
-            for (itemStack in player.inventory) {
-                if (itemStack == null || itemStack.type != Material.LEAD) {
-                        continue
-                }
-                itemStack.amount = itemStack.amount - 1
-                break
-            }
+            firstLoc.remove()
         }
     }
 
     @EventHandler
     fun onEntityInteract(event: PlayerInteractEntityEvent) {
-        if (event.rightClicked.type == EntityType.BAT && event.player.inventory.itemInMainHand.type != Material.LEAD) {
+        if (event.rightClicked.type == EntityType.BAT && event.player.inventory.itemInMainHand.type != Material.LEAD && !leashMap.containsKey(event.player)) {
+            if (event.player.gameMode != GameMode.CREATIVE) {
+            event.rightClicked.world.dropItemNaturally(event.rightClicked.location, ItemStack(Material.LEAD, 1))
+            }
             activeBats[event.rightClicked]?.remove()
             event.rightClicked.remove()
         }
@@ -113,8 +142,22 @@ class ItemClickListener(private val plugin: JavaPlugin) : Listener {
 
     @EventHandler
     fun onLeadBreak(event: EntityUnleashEvent) {
-        if (event.entity.type == EntityType.BAT) {
-            event.isCancelled = true
+        if (playerMap[event.entity]?.inventory?.itemInMainHand?.type == Material.LEAD || leashMap.containsKey(playerMap[event.entity])) {event.isCancelled = true; return}
+        if (event.entity.type == EntityType.BAT && playerMap[event.entity] != null) {
+            leashMap.remove(playerMap[event.entity])
+            event.entity.remove()
+        }
+    }
+
+    @EventHandler
+    fun entityRemoved(event: EntityUnleashEvent) {
+        if (event.entity is LeashHitch) {
+            val hitch = event.entity
+            val nearbyItems = hitch.world.getNearbyEntities(hitch.location, 1.0,1.0,1.0)
+                .filterIsInstance<org.bukkit.entity.Item>()
+                .filter { it.itemStack.type == Material.LEAD }
+
+            nearbyItems.forEach { it.remove() }
         }
     }
 }
